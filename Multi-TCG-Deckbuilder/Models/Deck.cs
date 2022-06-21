2337332ebec18ca759b119d131e7c9d4aa156dbd
Eronan <animeftw@live.co.uk>
2022-06-21 22:13:17 +1000

    Add project files.

---
+++ Multi-TCG-Deckbuilder/Models/Deck.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Multi_TCG_Deckbuilder.Models
+{
+    internal class Deck
+    {
+        string name;
+        string longName;
+        int minSize;
+        int maxSize;
+    }
+}
---
+++ Multi-TCG-Deckbuilder/Models/Game.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//using Newtonsoft.Json.Linq;
+
+namespace Multi_TCG_Deckbuilder.Models
+{
+    internal class Game
+    {
+        string name;
+        string longName;
+        string version;
+        int defaultMaxCopies;
+        Format[] formats;
+        dynamic cardlist;
+        dynamic deckSizes;
+
+        public Game()
+        {
+
+        }
+    }
+}
